89452f7bffd9bd60114bdc6e3f41ce01862f7a96
Mike Casto <castoware@gmail.com>
2020-09-27 09:02:45 -0400

    Added osx-brightness

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,23 @@
 #!/usr/local/bin/node
 process.stdin.resume();
 
-process.on('SIGINT', (code) => {
-  volume.unmute();
-  exec(`brightness 1`);
-  process.exit(0);
-})
+const brightness = require("osx-brightness");
+const volume = require("osx-volume");
+const { argv } = require("yargs");
 
-const { exec } = require('child_process');
-const volume = require('osx-volume');
-const {argv} = require('yargs');
+process.on("SIGINT", (code) => {
+  volume.unmute();
+  brightness.set(1);
+  process.exit(0);
+});
 
 const hours = argv.h || argv.hours || 8;
-const brightness = .1 * (argv.b || argv.brightness || argv.bright || 5);
+const level = 0.1 * (argv.b || argv.brightness || argv.bright || 5);
 
 volume.mute();
-exec(`brightness ${brightness}`);
+brightness.set(level);
 
 setTimeout(() => {
   volume.unmute();
-  exec(`brightness 1`);
-}, (hours * 1000*60*60));
+  brightness.set(1);
+}, hours * 1000 * 60 * 60);
